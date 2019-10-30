d2d3f8b3d309bcbc284c22a90a5d757653ccbb53
iden-alex <50949692+iden-alex@users.noreply.github.com>
2019-10-31 01:15:29 +0300

    Update matrix.h

--- 03/matrix.h
+++ 03/matrix.h
@@ -6,6 +6,13 @@ class Row {
 public:    
     Row(size_t n, int *arr_row): cols(n), ptr(arr_row) {}
     ~Row() {}
+    int operator [](size_t j) const {
+        if (j < cols) {
+            return *(ptr + j);
+        } else {
+            throw std::out_of_range("");
+        }
+    }
     int& operator [](size_t j) {
         if (j < cols) {
             return *(ptr + j);
@@ -15,18 +22,46 @@ public:
     }
 };
 
-class Matrix 
-{
+class Matrix {
     size_t rows;
     size_t cols;
     int *ptr;
 public:
-    Matrix(size_t r, size_t c):  rows(r), cols(c), 
-        ptr((int*)malloc(r * c * sizeof(int))) {}
+    Matrix(size_t r, size_t c):  rows(r), cols(c), ptr(new int[r*c]) {}
     ~Matrix() {
-        if (ptr) {
+        if (ptr != nullptr) {
+            delete [] ptr;
+        }
+    }
+    Matrix(const Matrix &B): rows(B.getRows()), cols(B.getColumns()),
+        ptr(new int[rows*cols]) 
+    {
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < cols; ++j) {
+                ptr[i*cols + j] = B.getElem(i, j);
+            }
+        }
+        
+    }
+    Matrix& operator =(const Matrix &B) {
+        if (this == &B) {
+            return *this;
+        }
+        if (ptr != nullptr) {
             free(ptr);
         }
+        cols = B.getColumns();
+        rows = B.getRows();
+        ptr = new int[rows*cols];
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < cols; ++j) {
+                ptr[i*cols + j] = B.getElem(i, j);
+            }
+        }
+        return *this;
+    }
+    int getElem(int i, int j) const {
+        return ptr[i*cols + j];
     }
     size_t getRows() const {
         return rows;
@@ -34,26 +69,24 @@ public:
     size_t getColumns() const {
         return cols;
     }
-    Row operator[](size_t i) const{
+    const Row operator[](size_t i) const {
         if (i < rows) {
             return Row(cols, ptr + i*cols);
         } else {
             throw std::out_of_range("");
         }
     }
-    Matrix& operator =(Matrix &B) {
-        if (this == &B) {
-            return *this;
+    Row operator[](size_t i) {
+        if (i < rows) {
+            return Row(cols, ptr + i*cols);
+        } else {
+            throw std::out_of_range("");
         }
-        if (ptr) {
-            free(ptr);
-        }
-        cols = B.getColumns();
-        rows = B.getRows();
-        ptr = (int*)malloc(rows * cols);
+    }
+    Matrix& operator *=(const int k) {
         for (size_t i = 0; i < rows; ++i) {
             for (size_t j = 0; j < cols; ++j) {
-                (*this)[i][j] = B[i][j];
+                ptr[i*cols + j] *= k;
             }
         }
         return *this;
@@ -67,19 +100,11 @@ public:
         } 
         for (size_t i = 0; i < rows; ++i) {
             for (size_t j = 0; j < cols; ++j) {
-                if ((*this)[i][j] != B[i][j]) {
+                if (ptr[i*cols + j] != B.getElem(i, j)) {
                     return false;
                 }
             }
         }
-    }
-    Matrix& operator *=(const int k) {
-        for (size_t i = 0; i < rows; ++i) {
-            for (size_t j = 0; j < cols; ++j) {
-                (*this)[i][j] *= k;
-            }
-        }
-        return *this;
     }
     bool operator !=(const Matrix &B) const {
         return !(*this == B);
